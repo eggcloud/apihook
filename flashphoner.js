8148114ddf3a38d43b2457ff3485dbf13911d212
stkim <stkim@rsupport.com>
2019-12-16 17:34:36 +0900

    create log.txt

--- flashphoner.js
+++ flashphoner.js
@@ -1,5 +1,14 @@
 const http = require('http');
 const fs = require('fs');
+const util = require('util');
+const logFile = fs.createWriteStream('log.txt', { flags: 'w' });
+const logStdout = process.stdout;
+ 
+console.log = function () {
+    logFile.write(util.format.apply(null, arguments) + '\n');
+    logStdout.write(util.format.apply(null, arguments) + '\n');
+}
+console.error = console.log;
 
 http.createServer((req, res) => {
 
@@ -23,54 +32,15 @@ http.createServer((req, res) => {
         return req.on('end', () => {
                 console.log('--------------------------------------------------------------------');
                 console.log(req.method,req.url);
-                console.log(body);
+                console.log(JSON.stringify(JSON.parse(body), null, 4));
                 res.writeHead(200);
                 res.end(body);
                 });
-
-        /*
-        if (req.url === '/apps/RSApp/connect') {
-            let body = '';
-            req.on('data', (data) => { body += data; });
-            return req.on('end', () => {
-                    console.log('--------------------------------------------------------------------');
-                    console.log(req.method,req.url);
-                    console.log(body);
-                    res.writeHead(200);
-                    res.end(body);
-                    });
-        } else if (req.url === '/apps/RSApp/playStream') {
-        } else if (req.url === '/apps/RSApp/pauseStream') {
-        } else if (req.url === '/apps/RSApp/stopStream') {
-        } else if (req.url === '/apps/RSApp/publishStream') {
-        } else if (req.url === '/apps/RSApp/snapshot') {
-        } else if (req.url === '/apps/RSApp/unPublishStream') {
-
-        } else if (req.url === '/apps/RSApp/ConnectionStatusEvent') {
-        } else if (req.url === '/apps/RSApp/StreamStatusEvent') {
-        } else if (req.url === '/apps/RSApp/StreamKeepAliveEvent') {
-        } else if (req.url === '/apps/RSApp/OnDataEvent') {
-
-        } else if (req.url === '/apps/RSApp/call') {
-        } else if (req.url === '/apps/RSApp/sendDtmf') {
-        } else if (req.url === '/apps/RSApp/answer') {
-        } else if (req.url === '/apps/RSApp/hold') {
-        } else if (req.url === '/apps/RSApp/unhold') {
-        } else if (req.url === '/apps/RSApp/transfer') {
-        } else if (req.url === '/apps/RSApp/hangup') {
-        } else if (req.url === '/apps/RSApp/submitBugReport') {
-        } else if (req.url === '/apps/RSApp/pushLogs') {
-        } else if (req.url === '/apps/RSApp/sessionDebug') {
-        } else if (req.url === '/apps/RSApp/SessionDebugStatusEvent') {
-        } else if (req.url === '/apps/RSApp/CallStatusEvent') {
-        } else if (req.url === '/apps/RSApp/RegistrationStatusEvent') {
-        }
-        */
     }
     res.writeHead(404, 'NOT FOUND');
     return res.end('NOT FOUND');
 
-}).listen(8443, () => {
-    console.log('Listening 8443 port');
+}).listen(8083, () => {
+    console.log('Listening 8083 port');
 });
 
